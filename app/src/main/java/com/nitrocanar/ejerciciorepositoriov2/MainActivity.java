999683c8832dce58e86db44ad7c690175a85bbf4
CRISTHIAN <criacamu@gmail.com>
2019-07-22 09:17:13 -0500

    creacion de botones de los integrantes

--- app/src/main/java/com/nitrocanar/ejerciciorepositoriov2/MainActivity.java
+++ app/src/main/java/com/nitrocanar/ejerciciorepositoriov2/MainActivity.java
@@ -3,12 +3,27 @@ package com.nitrocanar.ejerciciorepositoriov2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnDavid:
+                break;
+            case R.id.btnEdwin:
+                break;
+            case R.id.btnDaniel:
+                break;
+            case R.id.btnCristhian:
+                break;
+        }
     }
 }
